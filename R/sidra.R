6db2ccd33b3cbe4a410750977b78d2299c40eb01
Rodrigo Emmanuel Santana Borges <rodrigo@borges.net.br>
2024-11-11 11:50:32 -0300

    fix typo

--- R/sidra.R
+++ R/sidra.R
@@ -171,7 +171,7 @@ sidra <- function (tabela, classificador="",
     lapply(1:length(res),\(x) arrumado(x))
 
   )
-  res$valor <- supressWarnings(as.numeric(res$valor))
+  res$valor <- SupressWarnings(as.numeric(res$valor))
   return(res)
 
 }
