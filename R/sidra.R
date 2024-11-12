fab4dd5236902fb5064c13e5f6ec0e5f3efda633
Rodrigo Emmanuel Santana Borges <rodrigo@borges.net.br>
2024-11-11 22:35:42 -0300

    optimizing main function for passing examples execution

--- R/sidra.R
+++ R/sidra.R
@@ -16,11 +16,9 @@
 #' PAM <- sidra(1612, 81)
 
 sidra <- function (tabela, classificador="",
-                   filtro_cats , nivel = min(tab_niveis(tabela)$nivel.id),
+                   filtro_cats , nivel = "N1",
                    filtro_niveis,
-                   periodo =
-                     paste0(min(tab_periodos(tabela)),"-",
-                            max(tab_periodos(tabela))), variavel = "all",
+                   periodo = "all", variavel = "all",
                    inicio, fim,part=F)
 {
   if (length(tabela) > 1) {
@@ -30,17 +28,22 @@ sidra <- function (tabela, classificador="",
   if (!tabela %in% sidra::sidrameta$id) {
     stop("A tabela informada n\u00e3o \u00e9 v\u00e1lida", call. = FALSE)
   }
-  if (!missing(inicio) && !missing(fim)) {
-    periodo <- paste0(inicio, "-", fim)
+
+  ##obtendo metadados única vez
+  metatab <- tab_meta(tabela)
+
+  #Determinação do período de forma vetorizada
+  perido <-   if (!missing(inicio) && !missing(fim)) {
+    paste0(inicio, "-", fim)
   } else if (missing(fim) && !missing(inicio)) {
-    periodo <- tab_periodos(tabela)
-    periodo <- periodo[periodo >= inicio]
+    metatab$periodo[metatab$periodo >= inicio]
   } else if (missing(inicio) && !missing(fim)) {
-    periodo <- tab_periodos(tabela)
-    periodo <- periodo[periodo<=fim]
+    metatab$periodo[metatab$periodo <= inicio]
   } else {
-    periodo <- paste0(periodo,collapse="|")
+    paste0(periodo,collapse="|")
   }
+
+  #Validando e ajustando `filtro_niveis` e `filtro_cats`
   if (!missing(filtro_niveis) && length(nivel) != length(filtro_niveis)) {
     stop("O argumento filtro_niveis, quando especificado, deve ser uma lista de
          mesmo tamanho que o argumento n\u00edvel",
@@ -54,63 +57,73 @@ sidra <- function (tabela, classificador="",
          call. = FALSE)
   }
 
+  # Função interna para concatenar elementos
   concav <- \(x) paste0(x,collapse=",")
-  if(!missing(filtro_cats)) {
-    classifs <- paste0(paste0(classificador,"[",lapply(filtro_cats,concav),"]"),collapse="|")
+
+  # Preparação de classificadores e localidades
+  classifs <- if(!missing(filtro_cats)) {
+    paste0(paste0(classificador,"[",sapply(filtro_cats,concav),"]"),collapse="|")
   } else if(length(classificador)==0){
-    classifs <- paste0(classificador,"[all]",collapse="|")
+    paste0(classificador,"[all]",collapse="|")
   } else {
-    classifs <- classificador
-  }
-  if(!missing(filtro_niveis)) {
-    locais <- paste0(paste0(nivel,"[",lapply(filtro_niveis,concav),"]"),collapse="|")
-  } else {
-    locais <- paste0(nivel,"[all]",collapse="|")
+    classificador
   }
 
+  locais <- if(!missing(filtro_niveis)) {
+    paste0(paste0(nivel,"[",sapply(filtro_niveis,concav),"]"),collapse="|")
+  } else {
+    paste0(nivel,"[all]",collapse="|")
+  }
+
+  # Concatenando variáveis de forma otimizada
+  variavel <-
   if(length(variavel)>1) {
-    variavel <- paste0(variavel,collapse="|")
-  }
+    paste0(variavel,collapse="|")
+  } else {variavel}
 
 
+  #Construindo URL
   base_url <- "https://servicodados.ibge.gov.br/api/v3/agregados/"
   url <- paste0(base_url, tabela,
                 "/periodos/", periodo,
                 "/variaveis/", variavel,
                 "?classificacao=", classifs,
                 "&localidades=", locais)
-  print(url)
+
   # C\u00e1lculo do tamanho da requisi\u00e7\u00e3o:
-  if(!grepl("-",periodo[1])) {
-    ntemps <- length(periodo)
+  ntemps <- if(is.character(periodo) && !grepl("-",periodo[1])) {
+    length(periodo)
   } else if (length(periodo)>1){
-    ntemps <- length(periodo[periodo >=inicio & periodo<=fim])
+    length(periodo[periodo >=inicio & periodo<=fim])
   } else {
-    ntemps <- length(tab_periodos(tabela))
+    length(metatab$periodos)
   }
 
-  if(!missing(filtro_cats)){
-    ncats <- sum(sapply(1:length(filtro_cats),\(x){nrow(filtro_cats[[x]])}),na.rm=T)
+  # Definindo ncats
+  ncats <- if(!missing(filtro_cats)){
+    ncats <- sum(sapply(filtro_cats,nrow),na.rm=T)
   } else {
-    clg <- tab_class(tabela)
+    class_esc <-
     if(classificador!="") {
-      sbclg <- names(clg)[grepl(paste0("-",classificador,"$"),names(clg))]
-      class_esc <- clg[sbclg]} else {
-        class_esc <- clg
+      metatab$classificacoes[grepl(paste0("-",classificador,"$"),names(metatab$classificacoes))]
+      } else {
+        metatab$classificacoes
       }
 
-    ncats <- sum(sapply(1:length(class_esc),\(x) {nrow(class_esc[[x]])}),na.rm=T)
+    sum(sapply(class_esc,nrow),na.rm=T)
   }
 
+  #Definindo nlocs
+  nlocs <-
   if(!missing(filtro_niveis)){
-    nlocs <- sum(sapply(1:length(filtro_niveis),\(x){length(filtro_niveis[[x]])}),na.rm=T)
+    sum(sapply(filtro_niveis,length),na.rm=T)
   } else {
-    loc_esc <- tab_niveis(tabela)[tab_niveis(tabela)$nivel.id %in% nivel]
-    nlocs <- nrow(loc_esc)
+    nvl <- tab_niveis(tabela)
+    nlocs <- nrow(nvl[nvl$nivel.id %in% nivel])
   }
 
+  # Calculando tamanho da consulta e particionando se necessário
   tamanho <- length(variavel)*ntemps*nlocs*ncats
-  print(tamanho)
 
   if (tamanho>1e5 & part) {
     message(paste(
@@ -118,12 +131,12 @@ sidra <- function (tabela, classificador="",
       "Vamos contornar este problema fazendo v\u00e1rias solicita\u00e7\u00f5es menores.",
       "Haver\u00e1 maior demora", sep = "\n"))
 
-    periodos <- tab_periodos(tabela)
+    periodos <- metatab$periodos
     requisicoes <- (tamanho %/% 100000) + 1
 
     cada <- periodos |> split(cut(seq_along(periodos), requisicoes)) |>
       lapply(range) |> sapply(paste0, collapse = "-")
-    print(cada)
+
 
     partes <- data.table::rbindlist(lapply(cada, sidra,
                                            tabela = tabela, classificador = classificador,
@@ -149,26 +162,27 @@ sidra <- function (tabela, classificador="",
 
   res <- rjson::fromJSON(json_data)
 
+  #Transformando json
   arrumado <- \(x) {
-    amplia_colunas <- cbind(id=res[[x]]$id,variavel=res[[x]]$variavel,
-                            periodo = names(res[[x]]$resultados[[1]]$series[[1]]$serie),
-                            do.call(cbind,lapply(1:length(res[[x]]$resultados),
+    cbind(id=x$id,variavel=x$variavel,
+                            periodo = names(x$resultados[[1]]$series[[1]]$serie),
+                            do.call(cbind,lapply(1:length(x$resultados),
                                                  \(y) {
                                                    df <- data.frame(
-                                                     valor=unlist(res[[x]]$resultados[[y]]$series[[1]]$serie))
+                                                     valor=unlist(x$resultados[[y]]$series[[1]]$serie))
                                                    names(df) <- paste0(
-                                                     paste0(res[[x]]$resultados[[y]]$classificacoes[[1]][c("id","nome")],
+                                                     paste0(x$resultados[[y]]$classificacoes[[1]][c("id","nome")],
                                                             collapse="|"),"|",
-                                                     res[[x]]$resultados[[y]]$classificacoes[[1]][["categoria"]][[1]],
+                                                     x$resultados[[y]]$classificacoes[[1]]$categoria[[1]],
                                                      "|",
-                                                     res[[x]]$resultados[[y]]$series[[1]]$localidade$nome)
+                                                     x$resultados[[y]]$series[[1]]$localidade$nome)
                                                    df})))|>
       tidyr::pivot_longer(-(1:3),names_to = c("cod_class","classificador","categoria","local"),
                           names_sep="\\|",values_to="valor")
 
   }
   res <- data.table::rbindlist(
-    lapply(1:length(res),\(x) arrumado(x))
+    lapply(res,\(x) arrumado(x))
 
   )
   res$valor <- base::suppressWarnings(as.numeric(res$valor))
