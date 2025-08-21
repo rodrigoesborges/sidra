7d6cfb71fa413f64a2619dde7f7e393354aa2779
Rodrigo Borges <rodrigo@borges.net.br>
2025-08-21 21:12:28 +0200

    v0.17 test adjust

--- R/tab_agr.R
+++ R/tab_agr.R
@@ -23,7 +23,7 @@ tab_agr <- \(agregado) {
   x <- substr(agregado,2,nchar(agregado))
   rota <- sidra::agregados[sidra::agregados$id==rc,]$rota
   url <- paste0(baseag,"?",rota,"=",x)
-  resp <- call_ibge({httr::GET(url)})
+  resp <- sidra:::call_ibge({httr::GET(url)})
   if (is.null(resp)){
     return(invisible(NULL))
   }
--- tests/testthat/test-checa-saida.R
+++ tests/testthat/test-checa-saida.R
@@ -6,5 +6,9 @@ library(sidra)       # load our package
 # Test whether the output is a data frame
 test_that("sidra(1705) returns a data frame", {
   output_table <- sidra(1705)
-  expect_is(output_table, "data.frame")
-})
+  if(is.null(sidra:::call_ibge({httr::GET('https://servicodados.ibge.gov.br/api/v3/agregados')}))) {
+    expect_null(output_table)
+  } else {
+  expect_type(output_table, "data.frame")
+  succeed("API was reachable and a data.frame was returned.")
+}})
